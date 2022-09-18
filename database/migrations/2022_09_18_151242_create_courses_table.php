4b202e063afd820a41aa0d3d01dc4128ac01bedd
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-09-18 18:15:34 +0300

    Add course fields

---
+++ database/migrations/2022_09_18_151242_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->text('course_name');
+            $table->text('course_school_category');
+            $table->text('course_photo');
+            $table->string('course_level');
+            $table->string('course_years');
+            $table->text('course_price_range');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
