5b5a223a75de934dd2da7a986481018fc88d2d7d
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-10-01 01:21:47 +0300

    Refactor code

--- resources/views/frontend/pages_frontend/events/index.blade.php
+++ resources/views/frontend/pages_frontend/events/index.blade.php
@@ -5,5 +5,144 @@
 @endsection
 
 @section('content')
+
+ <!-- Breadcrumbs Start -->
+            <div class="rs-breadcrumbs breadcrumbs-overlay">
+                <div class="breadcrumbs-img">
+                    <img src="assets/frontend/images/breadcrumbs/2.jpg" alt="Breadcrumbs Image">
+                </div>
+                <div class="breadcrumbs-text white-color">
+                    <h1 class="page-title">IPI - Events </h1>
+                    <ul>
+                        <li>
+                            <a class="active" href="index2.html">IPI</a>
+                        </li>
+                        <li>Events </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Breadcrumbs End -->            
+
+    		<!-- Events Section Start -->
+            <div class="rs-event modify2 orange-color pt-100 pb-100 md-pt-70 md-pb-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 mb-30 col-md-6">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/frontend/images/event/style3/1.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Kabaka's Birthday Run</a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> Kampala</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   
+                        <div class="col-lg-4 mb-30 col-md-6">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/frontend/images/event/style3/2.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Computer literacy </a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> IPI Main Campus</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   
+                        <div class="col-lg-4 mb-30 col-md-6">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/frontend/images/event/style3/3.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Free Community Health Checks</a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> Maya Town</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   
+                        <!-- <div class="col-lg-4 col-md-6 md-mb-30">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/images/event/style3/4.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Job Seekers From Overcoming Failure</a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   
+                        <div class="col-lg-4 col-md-6 sm-mb-30">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/images/event/style3/5.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Best Technology Graduation Ceremony.</a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   
+                        <div class="col-lg-4 col-md-6">
+                            <div class="event-item">
+                                <div class="event-short">
+                                   <div class="featured-img">
+                                       <img src="assets/images/event/style3/6.jpg" alt="Image">
+                                   </div>
+                                   <div class="content-part">
+                                       <h4 class="title"><a href="#">Educational Technology and Mobile Learning</a></h4>
+                                       <div class="address"><i class="fa fa-map-o"></i> New Margania</div>
+                                        <div class="date-part">
+                                            <div class="date">
+                                                <i class="fa fa-calendar-check-o"></i>July 24, 2020  
+                                            </div>
+                                        </div>
+                                        <div class="time"><i class="fa flaticon-clock"></i> 11:00 AM - 03:00 AM</div>
+                                   </div> 
+                                </div>
+                            </div>
+                        </div>   -->
+                    </div>
+                </div> 
+            </div>
     
 @endsection
--- resources/views/frontend/pages_frontend/nursing/index.blade.php
+++ resources/views/frontend/pages_frontend/nursing/index.blade.php
@@ -5,5 +5,119 @@
 @endsection
 
 @section('content')
-    
+         <!-- Banner Section Start -->            
+           <div id="rs-banner" class="rs-banner style7">
+            <div class="container">
+                 <div class="row">
+                     <div class="col-lg-6">
+                         <div class="banner-content">
+                             <h1 class="banner-title white-color">Welcome to IPI Nursing School</h1>
+                             <div class="desc white-color mb-50">
+                                 We Train students in Nursing Sciences
+                             </div>
+                             <div class="btn-part">
+                                 <a class="readon border-less" href="#">Get Started Now</a>
+                             </div>
+                         </div>
+                         <div class="icons one up-down">
+                             <img src="assets/frontend/images/banner/home7/icon/2.png" alt="Images">
+                         </div> 
+                         <div class="icons two left-right">
+                             <img src="assets/frontend/images/banner/home7/icon/1.png" alt="Images">
+                         </div> 
+                         <div class="icons three left-right">
+                             <img src="assets/frontend/images/banner/home7/icon/1.png" alt="Images">
+                         </div> 
+                         <div class="icons four up-down">
+                             <img src="assets/frontend/images/banner/home7/icon/2.png" alt="Images">
+                         </div>
+                     </div>
+                     <div class="offset-lg-6"></div>
+                 </div>
+            </div>
+            <div class="img-part">
+                <img src="assets/frontend/images/banner/home7/nursing.png" style=" box-shadow: 0 0 100px 100px green inset;" alt="Images">
+            </div>
+        </div>
+         <!-- Banner Section End -->
+
+            <!-- Breadcrumbs End -->
+
+            <!-- Popular Courses Section Start -->
+            <div class="rs-latest-couses orange-color pt-100 pb-100 md-pt-70 md-pb-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 mb-40">
+                            <div class="course-item">
+                                <div class="course-image">
+                                  <a href="#">
+                                      <img src="assets/frontend/images/courses/home6/style1/7.png" alt="images">
+                                  </a>
+                                </div>
+                                <div class="course-info">
+                                 <ul class="meta-part">
+                                     <li><span class="price">1,500,000 UGX</span></li>
+                                     <!-- <li class="user"><i class="fa fa-user"></i> 245</li> -->
+                                 </ul>
+                                    <h3 class="course-title">
+                                        <a href="course-single.html">
+                                            Spacious and world class facility for Nusing Students
+                                        </a>
+                                    </h3>
+                                    <div class="bottom-part">
+                                        <!-- <div class="info-meta">
+                                            <ul>                                                
+                                                <li class="ratings">
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    (05)
+                                                </li>
+                                            </ul>
+                                        </div> -->
+                                        <div class="btn-part">
+                                            <a href="nursing-details.html">View Details<i class="flaticon-right-arrow"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 mb-40">
+                            <div class="course-item">
+                                <div class="course-image">
+                                  <a href="course-single.html">
+                                      <img src="assets/frontend/images/courses/home6/style1/8.png" alt="images">
+                                  </a>
+                                </div>
+                                <div class="course-info">
+                                 <ul class="meta-part">
+                                     <li><span class="price">1,500,000 UGX</span></li>
+                                     <!-- <li class="user"><i class="fa fa-user"></i> 245</li> -->
+                                 </ul>
+                                    <h3 class="course-title">
+                                        <a href="course-single.html">Highly competent facilitators do train our students</a>
+                                    </h3>
+                                    <div class="bottom-part">
+                                        <!-- <div class="info-meta">
+                                            <ul>                                                
+                                                <li class="ratings">
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    (05)
+                                                </li>
+                                            </ul>
+                                        </div> -->
+                                        <div class="btn-part">
+                                            <a href="nursing-details.html">View Details<i class="flaticon-right-arrow"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     
+                    </div>
+                </div>
+            </div>
+            <!-- Popular Courses Section End -->
 @endsection
