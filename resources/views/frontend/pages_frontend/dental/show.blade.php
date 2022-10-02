ba9f7b6812c095cadcdf0c562a124a4cf884a93d
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-10-03 01:34:31 +0300

    Fetch single page nursing

--- resources/views/frontend/pages_frontend/dental/show.blade.php
+++ resources/views/frontend/pages_frontend/dental/show.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts_frontend.master')
 
 @section('title')
-    Nursing
+    Course Details
 @endsection
 
 @section('content')
--- resources/views/frontend/pages_frontend/nursing/show.blade.php
+++ resources/views/frontend/pages_frontend/nursing/show.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts_frontend.master')
 
 @section('title')
-    Nursing
+    Course Details
 @endsection
 
 @section('content')
@@ -16,7 +16,7 @@
 								<li>
 									<a class="active" href="index-2.html">Home</a>
 								</li>
-								<li>Nursing</li>
+								<li>Dental</li>
 							</ul>
 					</div>
 			</div>
@@ -30,42 +30,25 @@
                         <div class="image-column col-lg-5 md-mb-50">
                             <div class="inner-column mb-50 md-mb-0">
                                 <div class="image">
-                                    <img src="{{ asset('assets/frontend/images/banner/home7/nursing.png') }}" alt="" />
+                                    <img src="{{ asset($course->course_photo) }}" alt="" />
                                 </div>
-                                <!-- <div class="team-content text-center">
-                                    <h3>Nursing</h3>
-                                    <div class="text">Chief Instructor</div>
-                                    <ul class="personal-info">
-                                        <li class="email">
-                                            <span><i class="glyph-icon flaticon-email"> </i> </span>
-                                            <a href="mailto:info@yourwebsite.com">info@yourwebsite.com</a>
-                                        </li>
-                                        <li class="phone">
-                                            <span><i class="glyph-icon flaticon-call"></i></span>
-                                            <a href="tel:+088-589-8745">(+088) 589-8745</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="social-box">
-                                    <a href="#" class="fa fa-facebook-square"></a>
-                                    <a href="#" class="fa fa-twitter-square"></a>
-                                    <a href="#" class="fa fa-linkedin-square"></a>
-                                    <a href="#" class="fa fa-github"></a>
-                                </div> -->
+                               
                             </div>
                         </div>                      
                         <!-- Content Section -->
                         <div class="content-column col-lg-7 pl-60 pt-50 md-pl-15 md-pt-0">
                             <div class="inner-column">
-                                <h2>Nursing School</h2>
-                                <h4>Category: Nursing and Midwifery</h4>
+                                <h2>School : {{ $course->course_school_category	 }}</h2>
+                                <h4>Course: {{ $course->course_name	 }}</h4>
                                 <!-- Student List -->
                                 <ul class="student-list">
-                                    <li>Duration: 3 years</li>
-                                    <li>Course Level: Diploma</li>
+                                    <li>Duration: {{ $course->course_years	 }}</li> <br>
+                                    <li>Course Level: {{ $course->course_level	 }}</li><br>
+                                    <li>Course Price: {{ $course->course_price_range	 }}</li><br>
+                                    <li>Course Level: {{ $course->course_level	 }}</li>
                                 </ul>
                                 <h5>Course Description</h5>
-                                <p>This course equips students with the necessary skills to work as nurses and midwives in hopitals, health centres and clinics</p>
+                                <p>{{ $course->description }}</p>
                                 
                             </div>
                         </div>
