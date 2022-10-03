b799dc0db5ea448a2685d098886e904639e3ed2e
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-10-04 01:41:04 +0300

    Refactor nav

--- resources/views/frontend/includes_frontend/header_frontend.blade.php
+++ resources/views/frontend/includes_frontend/header_frontend.blade.php
@@ -57,47 +57,14 @@
                                       </div>
                                       <nav class="rs-menu">
                                          <ul class="nav-menu">
-                                            <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item"> <a href="/">Home</a>
-                                                <ul class="mega-menu"> 
-                                                    <!-- <li class="mega-menu-container">
-                                                        <div class="mega-menu-innner">
-                                                            <div class="single-megamenu">
-                                                                <ul class="sub-menu">
-                                                                    <li><a href="index-2.html">Main Demo</a> </li>
-                                                                    <li class="active"><a href="index2.html">Online Course</a> </li>
-                                                                    <li><a href="index3.html">University 01</a> </li>
-                                                                    <li><a href="index4.html">E-Learning</a> </li>
-                                                                    <li><a href="index5.html">Distance Learning</a> </li>
-                                                                </ul>
-                                                            </div>
-                                                            <div class="single-megamenu">
-                                                                <ul class="sub-menu last-sub-menu">
-                                                                    <li><a href="index6.html">Personal Demo</a> </li>
-                                                                    <li><a href="index7.html">Online Training</a> </li>
-                                                                    <li><a href="index8.html">Online Learning</a> </li>
-                                                                    <li><a href="index9.html">Kitchen Coach</a> </li>
-                                                                    <li><a href="index10.html">University 02</a> </li>
-                                                                </ul>
-                                                            </div>  
-                                                            <div class="single-megamenu">
-                                                                <ul class="sub-menu last-sub-menu">
-                                                                    <li><a href="index11.html">Kindergarten 01</a> </li>
-                                                                    <li><a href="index12.html">Freelancing Course</a> </li>
-                                                                    <li><a href="index13.html">Gym Coach</a> </li>
-                                                                    <li><a href="index14.html">Courses Archive</a> </li>
-                                                                    <li><a href="index15.html">Courses Hub</a> </li>
-                                                                </ul>
-                                                            </div> 
-                                                        </div>
-                                                    </li> -->
-                                                </ul> 
-                                            </li>
+                                            <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item"> <a href="/">Home</a></li>
                                              <li class="menu-item-has-children">
                                                  <a href="#">School</a>
                                                  <ul class="sub-menu">
                                                     <li><a href="/about">About</a> </li>
                                                      <li><a href="/our_team">Staff</a> </li>
                                                      {{-- <li><a href="/our_faqs">FAQ</a> </li> --}}
+                                                      <li><a href="/our_events">Events</a></li>
                                                      <li><a href="/join_volunteers/create">Volunteer</a> </li>
                                                  </ul>
                                              </li>
@@ -109,54 +76,6 @@
                                                      <li><a href="/nursing">Nusing School</a> </li>
                                                      
                                                  </ul>
-                                             </li>
-
-                                             <li class="menu-item-has-children">
-                                                 <a href="/our_events">Events</a>
-                                                 <!-- <ul class="sub-menu">
-                                                     <li class="menu-item-has-children right">
-                                                         <a href="#">Team</a>
-                                                         <ul class="sub-menu right">
-                                                             <li><a href="team.html">Team One</a></li>
-                                                             <li><a href="team2.html">Team Two</a></li>
-                                                             <li><a href="team-single.html">Team Single</a></li>
-                                                         </ul>
-                                                     </li>
-                                                     <li class="menu-item-has-children">
-                                                         <a href="#">Event</a>
-                                                         <ul class="sub-menu right">
-                                                             <li><a href="events-style1.html">Event One</a></li>
-                                                             <li><a href="events-style2.html">Event Two</a></li>
-                                                             <li><a href="events-style3.html">Event Three</a></li>
-                                                         </ul>
-                                                     </li>
-                                                     <li class="menu-item-has-children">
-                                                         <a href="#">Gallery</a>
-                                                         <ul class="sub-menu right">
-                                                             <li><a href="gallery-style1.html">Gallery One</a></li>
-                                                             <li><a href="gallery-style2.html">Gallery Two</a></li>
-                                                             <li><a href="gallery-style3.html">Gallery Three</a></li>
-                                                         </ul>
-                                                     </li>
-                                                     <li class="menu-item-has-children">
-                                                         <a href="#">Shop</a>
-                                                         <ul class="sub-menu right">
-                                                             <li><a href="shop.html">Shop</a></li>
-                                                             <li><a href="shop-single.html">Shop Single</a></li>
-                                                             <li><a href="cart.html">Cart</a></li>
-                                                             <li><a href="checkout.html">Checkout</a></li>
-                                                         </ul>
-                                                     </li>
-                                                     <li class="menu-item-has-children">
-                                                         <a href="#">Others</a>
-                                                         <ul class="sub-menu right">
-                                                             <li><a href="faq.html">FAQ</a></li>
-                                                             <li><a href="error.html">404 Page</a></li>
-                                                             <li><a href="login.html">Login</a></li>
-                                                             <li><a href="register.html">Register</a></li>
-                                                         </ul>
-                                                     </li>
-                                                 </ul> -->
                                              </li>
 
                                              <li class="menu-item-has-children">
@@ -184,7 +103,7 @@
                                              </li>
                                              <li class="menu-item-has-children">
                                                 <!-- <a href="#">Donate</a> -->
-                                                <a class="apply-btn" href="/donate/create">Donate</a>
+                                                <a class="apply-btn" href="/donate/create">Payments</a>
                                             </li>
                                          </ul> <!-- //.nav-menu -->
                                       </nav>                                         
